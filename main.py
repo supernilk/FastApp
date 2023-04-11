802c2cd90c451ca1e3105455791f2341b9d667e3
Snilk <nilk.ao@gmail.com>
2023-04-11 17:29:10 -0400

    Importante FastAppi crea automaticamente la documentacion a medida que se va creando el proyecto de forma automatica

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+#https://stackoverflow.com/questions/64936440/python-uvicorn-the-term-uvicorn-is-not-recognized-as-the-name-of-a-cmdlet-f
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return "hola FastApp"
+    return "Hola FastApp"
 
-#para ejecutar:
-#en caso de que no funcione con uvicorn main:app --reload
-#python -m uvicorn main:app --reload
+@app.get("/url")
+async def url():
+    return {"url_curso": "https://mouredev.com/python"}
+#Iniciar el server: uvicorn main:app --reload
+#En caso de que no funcione: py -m uvicorn main:app --reload
+#Detener el server: CTRL+C
+
+#Documentacion con Swagger: http://127.0.0.1:8000/docs
+#Documentacion con Redocly: http://127.0.0.1:8000/redoc
+
+#
