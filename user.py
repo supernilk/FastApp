bb33e244d6c3758a86f79b5fb2a6d864963d4093
Snilk <nilk.ao@gmail.com>
2023-04-14 17:39:50 -0400

    testeando

--- user.py
+++ user.py
@@ -3,7 +3,12 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-#Iniciar el server: py -m uvicorn user:app --reload
+#-----------------  Iniciar el server  -----------------  
+#pyenv local 3.7.4
+#python -m venv venv
+#.\venv\Scripts\activate
+#py -m uvicorn user:app --reload
+
 
 #Entidad usuario
 class Usuario(BaseModel):
@@ -22,10 +27,23 @@ Usuarios = [Usuario(id=1, nombre="John", apellido="Barragan", url="html://google
 async def listar_usuarios():
     return Usuarios
 
-@app.get("/usuario/{id}")
+
+@app.get("/usuario/{ide}")# Path
+async def buscar_usuario(ide:int):
+    return buscar_usuario(ide)
+
+
+@app.get("/usuario/")# Query
 async def buscar_usuario(id:int):
+    return buscar_usuario(id)
+    
+def buscar_usuario(id:int):
     try:
         usuarios = filter(lambda user:user.id==id,Usuarios)
         return list(usuarios)[0]
     except:
-        return {"error":"no se ha encontradogit el usuario"}
+        return {"error":"no se ha encontrado el usuario"}
+
+@app.post("/usuario/")
+async def Agregar_usuario():
+    return ("agregando usuario")
